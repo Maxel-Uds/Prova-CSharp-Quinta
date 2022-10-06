3fff88932344a0d0ebf61612519dd0496df2df3c
Maxel-Uds <maxellopes32@gmail.com>
2022-10-06 20:46:51 -0300

    Prova terminada

---
+++ Migrations/20221006231446_NewPropertiesForFolhas.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API_FOlhas.Migrations
+{
+    public partial class NewPropertiesForFolhas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CriadoEm",
+                table: "Folhas",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "FuncionarioId",
+                table: "Folhas",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "ImpostoFgts",
+                table: "Folhas",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "ImpostoInss",
+                table: "Folhas",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "ImpostoRenda",
+                table: "Folhas",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "QuantidadeHoras",
+                table: "Folhas",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "SalarioBruto",
+                table: "Folhas",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "SalarioLiquido",
+                table: "Folhas",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "ValorHora",
+                table: "Folhas",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Folhas_FuncionarioId",
+                table: "Folhas",
+                column: "FuncionarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Folhas_Funcionarios_FuncionarioId",
+                table: "Folhas",
+                column: "FuncionarioId",
+                principalTable: "Funcionarios",
+                principalColumn: "FuncionarioId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Folhas_Funcionarios_FuncionarioId",
+                table: "Folhas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Folhas_FuncionarioId",
+                table: "Folhas");
+
+            migrationBuilder.DropColumn(
+                name: "CriadoEm",
+                table: "Folhas");
+
+            migrationBuilder.DropColumn(
+                name: "FuncionarioId",
+                table: "Folhas");
+
+            migrationBuilder.DropColumn(
+                name: "ImpostoFgts",
+                table: "Folhas");
+
+            migrationBuilder.DropColumn(
+                name: "ImpostoInss",
+                table: "Folhas");
+
+            migrationBuilder.DropColumn(
+                name: "ImpostoRenda",
+                table: "Folhas");
+
+            migrationBuilder.DropColumn(
+                name: "QuantidadeHoras",
+                table: "Folhas");
+
+            migrationBuilder.DropColumn(
+                name: "SalarioBruto",
+                table: "Folhas");
+
+            migrationBuilder.DropColumn(
+                name: "SalarioLiquido",
+                table: "Folhas");
+
+            migrationBuilder.DropColumn(
+                name: "ValorHora",
+                table: "Folhas");
+        }
+    }
+}
